09a8595c1f814990aa30c3c592f4f5e8f7d376dc
JC <jcmpes@gmail.com>
2020-06-25 07:56:09 +0200

    Register models in admin.py

--- src/memberships/models.py
+++ src/memberships/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.db.models.sigals import post_save
+from django.db.models.signals import post_save
 
 import stripe
 
@@ -22,28 +22,28 @@ class Membership(models.Model):
 
 
 class UserMembership(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delte=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     stripe_customer_id = models.CharField(max_length=40)
     membership = models.ForeignKey(Membership, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.user.username
 
-    def post_save_usermembership_create(sender, instance, created, *args, **kwargs):
-        if created:
-            UserMembership.objects.get_or_create(user=instance)
+def post_save_usermembership_create(sender, instance, created, *args, **kwargs):
+    if created:
+        UserMembership.objects.get_or_create(user=instance)
 
-        user_membership, created = UserMembership.objects.get_or_create(user=instance)
+    user_membership, created = UserMembership.objects.get_or_create(user=instance)
 
-        if user_membership.stripe_customer_id is None or user_membership.stripe_customer_id == '':
-            new_customer_id = stripe.Customer.create(email=instance.email)
-            user_membership.stripe_customer_id = new_customer_id['id']
-            user_membership.save()
+    if user_membership.stripe_customer_id is None or user_membership.stripe_customer_id == '':
+        new_customer_id = stripe.Customer.create(email=instance.email)
+        user_membership.stripe_customer_id = new_customer_id['id']
+        user_membership.save()
 
-    post_save.connect(post_save_usermembership_create, sender=settings.AUTH_USER_MODEL)
+post_save.connect(post_save_usermembership_create, sender=settings.AUTH_USER_MODEL)
 
 class Subscription(models.Model):
-    user_membership = models.ForeignKey(UserMembership(on_delete=models.CASCADE)
+    user_membership = models.ForeignKey(UserMembership, on_delete=models.CASCADE)
     stripe_subscription_id = models.CharField(max_length=40)
     active = models.BooleanField(default=True)
 
